7472a865da3b6efd60369d7f95bdb153dc027310
Ian Brun <crackedcomputerstudios@gmail.com>
2020-09-12 07:44:36 +0200

    Added include guards to 'collision_set.h' See https://github.com/ValveSoftware/source-sdk-2013/issues/228

--- sp/src/public/vphysics/collision_set.h
+++ sp/src/public/vphysics/collision_set.h
@@ -6,6 +6,12 @@
 //
 //=============================================================================//
 
+#ifndef COLLISION_SET_H
+#define COLLISION_SET_H
+#ifdef _WIN32
+#pragma once
+#endif
+
 // A set of collision rules
 // NOTE: Defaults to all indices disabled
 class IPhysicsCollisionSet
@@ -18,3 +24,5 @@ public:
 
 	virtual bool ShouldCollide( int index0, int index1 ) = 0;
 };
+
+#endif // COLLISION_SET_H
