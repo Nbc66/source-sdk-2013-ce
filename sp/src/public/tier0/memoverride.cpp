9908138c0672006c387d164dca2f5fd418a8f2b4
DotWithATopHat <52358442+DotWith@users.noreply.github.com>
2022-04-24 01:30:11 -0400

    Trying to merge it correctly

--- sp/src/public/tier0/memoverride.cpp
+++ sp/src/public/tier0/memoverride.cpp
@@ -174,6 +174,11 @@ extern "C"
     {
         return AllocUnattributed(nSize);
     }
+#elif _MSC_VER >= 1900
+    __declspec(restrict) void* _malloc_base(size_t nSize)
+    {
+        return AllocUnattributed(nSize);
+    }
 #else
     ALLOC_CALL void* _malloc_base(size_t nSize)
     {
@@ -181,32 +186,51 @@ extern "C"
     }
 #endif
 
+#if _MSC_VER >= 1900
+    __declspec(restrict) void* _calloc_base(size_t count, size_t nSize)
+    {
+        void* pMem = AllocUnattributed(count * nSize);
+        memset(pMem, 0, count * nSize);
+        return pMem;
+    }
+#else
     ALLOC_CALL void* _calloc_base(size_t nCount, size_t nSize)
     {
         void* pMem = AllocUnattributed(nCount * nSize);
         memset(pMem, 0, nCount * nSize);
         return pMem;
-}
+    }
+#endif
 
+#if _MSC_VER >= 1900
+    __declspec(restrict) void* _realloc_base(void* pMem, size_t nSize)
+    {
+        return ReallocUnattributed(pMem, nSize);
+    }
+#else
     ALLOC_CALL void* _realloc_base(void* pMem, size_t nSize)
     {
         return ReallocUnattributed(pMem, nSize);
     }
+#endif
 
+#if _MSC_VER >= 1900
+    __declspec(restrict) void* _recalloc_base(void* pMem, size_t count, size_t nSize)
+    {
+        return _recalloc(pMem, count, nSize);
+    }
+#else
     ALLOC_CALL void* _recalloc_base(void* pMem, size_t nCount, size_t nSize)
     {
         void* pMemOut = ReallocUnattributed(pMem, nCount * nSize);
         memset(pMemOut, 0, nCount * nSize);
         return pMemOut;
     }
+#endif
 
     void _free_base(void* pMem)
     {
-#if !defined(USE_LIGHT_MEM_DEBUG) && !defined(USE_MEM_DEBUG)
         g_pMemAlloc->Free(pMem);
-#else
-        g_pMemAlloc->Free(pMem, g_pszModule, 0);
-#endif
     }
 
     void* __cdecl _expand_base(void* pMem, size_t nNewSize, int nBlockUse)
@@ -223,7 +247,11 @@ extern "C"
 
     void* __cdecl _calloc_crt(size_t count, size_t size)
     {
+#if _MSC_VER >= 1900
         return _calloc_base(count, size);
+#else
+        return _calloc_base(count * size);
+#endif
     }
 
     void* __cdecl _realloc_crt(void* ptr, size_t size)
@@ -233,14 +261,23 @@ extern "C"
 
     void* __cdecl _recalloc_crt(void* ptr, size_t count, size_t size)
     {
-        return _recalloc_base(ptr, size, count);
+#if _MSC_VER >= 1900
+        return _recalloc_base(ptr, count, size);
+#else
+        return _recalloc_base(ptr, size * count);
+#endif
     }
 
     ALLOC_CALL void* __cdecl _recalloc(void* memblock, size_t count, size_t size)
     {
-        void* pMem = ReallocUnattributed(memblock, size * count);
-        memset(pMem, 0, size * count);
-        return pMem;
+        const size_t oldSize = _msize(memblock);
+        const size_t newSize = count * size;
+        void* pMemOut = ReallocUnattributed(memblock, newSize);
+
+        if (newSize > oldSize)
+            memset(((char*)pMemOut) + oldSize, 0, newSize - oldSize);
+
+        return pMemOut;
     }
 
     size_t _msize_base(void* pMem)noexcept
