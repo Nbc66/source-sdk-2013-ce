169840fdbd8972abeb80ea9f43c5cde138b8b919
Noodles <noodletrysoup@gmail.com>
2023-07-29 12:03:20 +1000

    Removed duplicated strtools functions. Fixed potential compile error
    I assume this was an issue with a particular Windows SDK version as  `_panelDar[ i ] == temp` would result in a C2666 error. I think this has been fixed as it no longer occurs, but I had encountered the issue in the past.

--- mp/src/vgui2/vgui_controls/BuildGroup.cpp
+++ mp/src/vgui2/vgui_controls/BuildGroup.cpp
@@ -856,7 +856,7 @@ void BuildGroup::PanelAdded(Panel *panel)
 	int c = _panelDar.Count();
 	for ( int i = 0; i < c; ++i )
 	{
-		if ( _panelDar[ i ] == temp )
+		if ( _panelDar[ i ].Get() == temp )
 		{
 			return;
 		}
